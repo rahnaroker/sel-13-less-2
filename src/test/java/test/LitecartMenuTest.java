f7ccfac7ff34546bce2f150d1ca3d365f82c1c84
rahnaroker <rahnar0ker@gmail.com>
2017-12-08 22:58:18 +0400

    7th task - loop test

--- src/test/java/test/LitecartMenuTest.java
+++ src/test/java/test/LitecartMenuTest.java
@@ -1,11 +1,13 @@
 package test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static main.DriverFactory.*;
 import static main.SettingsProvider.getRunXamppServer;
@@ -41,7 +43,33 @@ public class LitecartMenuTest {
     }
 
     @Test
-    public void checkMenuTree() {
+    public void checkMenuTree_Loop() {
+        getDriver().get("http://localhost/litecart/admin/");
+        getDriver().findElement(By.name("username")).sendKeys("admin");
+        getDriver().findElement(By.name("password")).sendKeys("admin");
+        getDriver().findElement(By.name("login")).click();
+        getWait().until(titleIs("My Store"));
+
+        List<WebElement> menus = getDriver().findElements(By.xpath("//ul[@id='box-apps-menu']/li"));
+
+        for (int i = 1; i <= menus.size(); i++)
+        {
+            WebElement menu = getDriver().findElement(By.xpath("//ul[@id='box-apps-menu']/li[" + i + "]/a"));
+            menu.click();
+            getDriver().findElement(By.cssSelector("h1"));
+
+            List <WebElement> subMenus = getDriver().findElements(By.xpath("//ul[@id='box-apps-menu']/li[" + i + "]/ul/li"));
+
+            for (int j = 1; j <= subMenus.size(); j++) {
+                WebElement subMenu = getDriver().findElement(By.xpath("//ul[@id='box-apps-menu']/li[" + i + "]/ul/li[" + j +"]"));
+                subMenu.click();
+                getDriver().findElement(By.cssSelector("h1"));
+            }
+        }
+    }
+
+    @Test
+    public void checkMenuTree_Line() {
         getDriver().get("http://localhost/litecart/admin/");
         getDriver().findElement(By.name("username")).sendKeys("admin");
         getDriver().findElement(By.name("password")).sendKeys("admin");
