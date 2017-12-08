0656210d1b13a017b9e7fe2413deef33e1bfe0d9
rahnaroker <rahnar0ker@gmail.com>
2017-12-09 00:56:04 +0400

    8th task - total update

--- src/test/java/test/LitecartGoodsTest.java
+++ src/test/java/test/LitecartGoodsTest.java
@@ -1,12 +1,14 @@
 package test;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 import static main.DriverFactory.*;
 import static main.SettingsProvider.getRunXamppServer;
@@ -31,19 +33,44 @@ public class LitecartGoodsTest {
     public void checkStickers() {
         getDriver().get("http://localhost/litecart/");
         getWait().until(titleIs("Online Store | My Store"));
-        /* Количество картинок с товарами совпадает с количеством стикеров на этих картинках */
-        Assert.assertEquals(getDriver().findElements(By.cssSelector("div.image-wrapper > img.image")).size(),
-                getDriver().findElements(By.cssSelector("div.image-wrapper > div.sticker")).size());
 
+        int numOfStickeres = 0;
+        getDriver().findElements(By.cssSelector("li.product")).equals(numOfStickeres);
+
+        List<WebElement> popularProducts = getDriver().findElements(By.cssSelector("div#box-most-popular li"));
+        for (int i = 1; i <= popularProducts.size(); i++)
+        {
+            WebElement popProduct = getDriver().findElement(By.cssSelector("div#box-most-popular li:nth-of-type(" + i + ") div.sticker"));
+            Assert.assertTrue(popProduct.isDisplayed());
+            numOfStickeres++;
+        }
+
+        List<WebElement> campaignsProducts = getDriver().findElements(By.cssSelector("div#box-campaigns li"));
+        for (int j = 1; j <= campaignsProducts.size(); j++)
+        {
+            WebElement campProduct = getDriver().findElement(By.cssSelector("div#box-campaigns li:nth-of-type(" + j + ") div.sticker"));
+            Assert.assertTrue(campProduct.isDisplayed());
+            numOfStickeres++;
+        }
+
+        List<WebElement> latestProducts = getDriver().findElements(By.cssSelector("div#box-latest-products li"));
+        for (int k = 1; k <= latestProducts.size(); k++)
+        {
+            WebElement latProduct = getDriver().findElement(By.cssSelector("div#box-latest-products li:nth-of-type(" + k + ") div.sticker"));
+            Assert.assertTrue(latProduct.isDisplayed());
+            numOfStickeres++;
+        }
+
+        System.out.println("Total Amount of stickered Products is " + numOfStickeres);
     }
 
     @AfterClass
     public void closeBrowser() {
-        stopBrowser();
         try {
             Runtime.getRuntime().exec(getStopXamppServer());
         } catch (IOException e) {
             e.printStackTrace();
         }
+        stopBrowser();
     }
 }
