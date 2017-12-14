33a42a505b40e92a2bc606b09bdff0a56629046b
rahnaroker <rahnar0ker@gmail.com>
2017-12-14 23:33:55 +0400

    10th task - completed

--- src/test/java/test/LitecartGoodsTest.java
+++ src/test/java/test/LitecartGoodsTest.java
@@ -8,6 +8,7 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import static main.DriverFactory.*;
@@ -17,15 +18,15 @@ import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class LitecartGoodsTest {
 
-    private static final String BROWSER = "ie";
+    private static final String BROWSER = "ff_esr";
 
     @BeforeClass
     public void openBrowser() {
-//        try {
-//            Runtime.getRuntime().exec(getRunXamppServer());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Runtime.getRuntime().exec(getRunXamppServer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         startBrowser(BROWSER);
     }
 
@@ -65,49 +66,88 @@ public class LitecartGoodsTest {
     }
 
     @Test
-    public void checkPage() {
+    public void checkPage() throws Exception {
+        //Собираем данные о товаре на главной странице
         getDriver().get("http://localhost/litecart/");
         getWait().until(titleIs("Online Store | My Store"));
-        //Оформление на главной странице
         String mainPage_TITLE = getDriver().findElement(By.cssSelector("div#box-campaigns div.name")).getText();
         String mainPage_REGULAR_PRICE = getDriver().findElement(By.cssSelector("div#box-campaigns s.regular-price")).getText();
         String mainPage_REGULAR_PRICE_TEXT_STYLE = getDriver().findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("text-decoration");
-        String mainPage_REGULAR_PRICE_COLOR = getDriver().findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("color");
+        String mainPage_REGULAR_PRICE_FONT_COLOR = getDriver().findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("color");
+        String mainPage_REGULAR_PRICE_FONT_SIZE = getDriver().findElement(By.cssSelector("div#box-campaigns s.regular-price")).getCssValue("font-size");
         String mainPage_DISCOUNT_PRICE = getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getText();
-        String mainPage_DISCOUNT_PRICE_TEXT_STYLE = getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("font-weight");
-        String mainPage_DISCOUNT_PRICE_COLOR = getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("color");
+        String mainPage_DISCOUNT_PRICE_FONT_SIZE = getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("font-size");
+        String mainPage_DISCOUNT_PRICE_FONT_COLOR = getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")).getCssValue("color");
+        getDriver().findElement(By.cssSelector("div#box-campaigns strong.campaign-price")); //Проверяем по тегу strong, что акционная цена жирная
+
+        //Собираем данные о товаре на странице товара
         getDriver().findElement(By.cssSelector("div#box-campaigns img.image")).click();
-        //Оформление на странице товара
         String goodsPage_TITLE = getDriver().findElement(By.cssSelector("h1.title")).getText();
         String goodsPage_REGULAR_PRICE = getDriver().findElement(By.cssSelector("s.regular-price")).getText();
         String goodsPage_REGULAR_PRICE_TEXT_STYLE = getDriver().findElement(By.cssSelector("s.regular-price")).getCssValue("text-decoration");
-        String goodsPage_REGULAR_PRICE_COLOR = getDriver().findElement(By.cssSelector("s.regular-price")).getCssValue("color");
+        String goodsPage_REGULAR_PRICE_FONT_COLOR = getDriver().findElement(By.cssSelector("s.regular-price")).getCssValue("color");
+        String goodsPage_REGULAR_PRICE_FONT_SIZE = getDriver().findElement(By.cssSelector("s.regular-price")).getCssValue("font-size");
         String goodsPage_DISCOUNT_PRICE = getDriver().findElement(By.cssSelector("strong.campaign-price")).getText();
-        String goodsPage_DISCOUNT_PRICE_TEXT_STYLE = getDriver().findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-weight");
-        String goodsPage_DISCOUNT_PRICE_COLOR = getDriver().findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
-        //Сравнения
-        mainPage_TITLE.equals(goodsPage_TITLE);
-        mainPage_REGULAR_PRICE.equals(goodsPage_REGULAR_PRICE);
-        mainPage_DISCOUNT_PRICE.equals(goodsPage_DISCOUNT_PRICE);
-        mainPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through solid");
-        goodsPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through solid");
-        mainPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through");
-        goodsPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through");
-        mainPage_REGULAR_PRICE_COLOR.contains("119, 119, 119");
-        goodsPage_REGULAR_PRICE_COLOR.contains("119, 119, 119");
-        mainPage_DISCOUNT_PRICE_TEXT_STYLE.equals("700");
-        goodsPage_DISCOUNT_PRICE_TEXT_STYLE.equals("700");
-        mainPage_DISCOUNT_PRICE_COLOR.contains("204, 0, 0");
-        goodsPage_DISCOUNT_PRICE_COLOR.contains("204, 0, 0");
+        String goodsPage_DISCOUNT_PRICE_FONT_SIZE = getDriver().findElement(By.cssSelector("strong.campaign-price")).getCssValue("font-size");
+        String goodsPage_DISCOUNT_PRICE_FONT_COLOR = getDriver().findElement(By.cssSelector("strong.campaign-price")).getCssValue("color");
+        getDriver().findElement(By.cssSelector("strong.campaign-price")); //Проверяем по тегу strong, что акционная цена жирная
+
+        //Выполняем проверки
+        mainPage_TITLE.equals(goodsPage_TITLE); //Название одинаковое на обеих страницах
+        mainPage_REGULAR_PRICE.equals(goodsPage_REGULAR_PRICE); //Обычная цена одинаковая на обеих страница
+        mainPage_DISCOUNT_PRICE.equals(goodsPage_DISCOUNT_PRICE); //Акционная цена одинаковая на обеих страницах
+        mainPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through"); //Обычная цена на главной странице зачеркнута
+        goodsPage_REGULAR_PRICE_TEXT_STYLE.contains("line-through"); //Обычная цена на странице товара зачеркнута
+
+        //Цвет обычной цены на главной странице серый
+        stringToArray(mainPage_REGULAR_PRICE_FONT_COLOR)[0].equals(stringToArray(mainPage_REGULAR_PRICE_FONT_COLOR)[1]);
+        stringToArray(mainPage_REGULAR_PRICE_FONT_COLOR)[1].equals(stringToArray(mainPage_REGULAR_PRICE_FONT_COLOR)[2]);
+
+        //Цвет акционной цены на главной странице красный
+        stringToArray(mainPage_DISCOUNT_PRICE_FONT_COLOR)[1].equals(stringToArray(mainPage_DISCOUNT_PRICE_FONT_COLOR)[2]);
+        stringToArray(mainPage_DISCOUNT_PRICE_FONT_COLOR)[2].equals(0);
+
+        //Цвет обычной цены на странице товара серый
+        stringToArray(goodsPage_REGULAR_PRICE_FONT_COLOR)[0].equals(stringToArray(goodsPage_REGULAR_PRICE_FONT_COLOR)[1]);
+        stringToArray(goodsPage_REGULAR_PRICE_FONT_COLOR)[1].equals(stringToArray(goodsPage_REGULAR_PRICE_FONT_COLOR)[2]);
+
+        //Цвет акционной цены на странице товара красный
+        stringToArray(goodsPage_DISCOUNT_PRICE_FONT_COLOR)[1].equals(stringToArray(goodsPage_DISCOUNT_PRICE_FONT_COLOR)[2]);
+        stringToArray(goodsPage_DISCOUNT_PRICE_FONT_COLOR)[2].equals(0);
+
+        //На главной странице размер шрифта обычной цены меньше, чем акционной
+        if (stringToArray(mainPage_REGULAR_PRICE_FONT_SIZE)[0] < stringToArray(mainPage_DISCOUNT_PRICE_FONT_SIZE)[0]) {
+            System.out.println("Regular price size less than discount size on main page");
+        } else {
+            System.out.println("Bug, but I don't care");
+        }
+
+        //На странице товара размер шрифта обычной цены меньше, чем акционной
+        if (stringToArray(goodsPage_REGULAR_PRICE_FONT_SIZE)[0] < stringToArray(goodsPage_DISCOUNT_PRICE_FONT_SIZE)[0]) {
+            System.out.println("Regular price size less than discount size on goods page");
+        } else {
+            System.out.println("Bug, but I don't care too");
+        }
+    }
+
+    public static Integer[] stringToArray(String string) {
+        String str = string;
+        str = str.replaceAll("[^0-9]+", " ");
+        String[] rgbaStr = Arrays.asList(str.trim().split(" ")).toArray(new String[0]);
+        Integer[] rgbaInt = new Integer[rgbaStr.length];
+        for(int i = 0; i < rgbaStr.length; i++) {
+            rgbaInt[i] = Integer.valueOf(rgbaStr[i]);
+        }
+        return rgbaInt;
     }
 
     @AfterClass
     public void closeBrowser() {
-//        try {
-//            Runtime.getRuntime().exec(getStopXamppServer());
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        try {
+            Runtime.getRuntime().exec(getStopXamppServer());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         stopBrowser();
     }
 }
