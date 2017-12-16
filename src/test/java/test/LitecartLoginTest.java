c122cf44e7c6eb0502bc4d241e9503b8ef874234
rahnaroker <rahnar0ker@gmail.com>
2017-12-17 01:58:53 +0400

    11th task

--- src/test/java/test/LitecartLoginTest.java
+++ src/test/java/test/LitecartLoginTest.java
@@ -1,12 +1,12 @@
 package test;
 
-import main.SettingsProvider;
 import org.openqa.selenium.By;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import static main.SettingsProvider.getRunXamppServer;
 import static main.SettingsProvider.getStopXamppServer;
@@ -15,7 +15,14 @@ import static main.DriverFactory.*;
 
 public class LitecartLoginTest {
 
+    private static final String EMAIL = "test" + generateString() + "@mail.com";
+    private static final String PASSWORD = generateString();
     private static final String BROWSER = "chrome";
+
+    public static String generateString() {
+        String uuid = UUID.randomUUID().toString();
+        return uuid;
+    }
 
     @BeforeClass
     public void openBrowser() {
@@ -34,6 +41,31 @@ public class LitecartLoginTest {
         getDriver().findElement(By.name("password")).sendKeys("admin");
         getDriver().findElement(By.name("login")).click();
         getWait().until(titleIs("My Store"));
+    }
+
+    @Test
+    public void createNewAccount() {
+        getDriver().get("http://localhost/litecart/");
+        getDriver().findElement(By.cssSelector("[name=login_form] a")).click();
+        getDriver().findElement(By.cssSelector("[name=firstname]")).sendKeys("John");
+        getDriver().findElement(By.cssSelector("[name=lastname]")).sendKeys("Doe");
+        getDriver().findElement(By.cssSelector("[name=address1]")).sendKeys("the Earth");
+        getDriver().findElement(By.cssSelector("[name=postcode]")).sendKeys("001002");
+        getDriver().findElement(By.cssSelector("[name=city]")).sendKeys("N");
+        getDriver().findElement(By.cssSelector("[name=email]")).sendKeys(EMAIL);
+        getDriver().findElement(By.cssSelector("[name=phone]")).sendKeys("+74815162342");
+        getDriver().findElement(By.cssSelector("[name=password]")).sendKeys(PASSWORD);
+        getDriver().findElement(By.cssSelector("[name=confirmed_password]")).sendKeys(PASSWORD);
+        getDriver().findElement(By.cssSelector("[name=create_account]")).click();
+        getDriver().findElement(By.cssSelector("div#notices > div")); //Заходим после создания учетной записи
+        getDriver().findElement(By.cssSelector("div#box-account li:last-child a")).click();
+        getDriver().findElement(By.cssSelector("div#notices > div")); //Выходим
+        getDriver().findElement(By.cssSelector("[name=email]")).sendKeys(EMAIL);
+        getDriver().findElement(By.cssSelector("[name=password]")).sendKeys(PASSWORD);
+        getDriver().findElement(By.cssSelector("[name=login]")).click();
+        getDriver().findElement(By.cssSelector("div#notices > div")); //Повторно заходим
+        getDriver().findElement(By.cssSelector("div#box-account li:last-child a")).click();
+        getDriver().findElement(By.cssSelector("div#notices > div")); //Повторно выходим
     }
 
     @AfterClass
